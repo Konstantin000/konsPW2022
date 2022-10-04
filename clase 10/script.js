6f5ccecf18e2984f29702bbded500611e53d7a41
Konstantin000 <kambrossenkov@itba.edu.ar>
2022-10-04 19:49:54 -0300

    Java

---
+++ clase 10/script.js
@@ -0,0 +1,85 @@
+console.log("hola mundo"); // comentario
+/* cometario multi lineal*/
+
+console.log(1+1)
+
+//LAS 3 VARIABLE:
+let nombre="Esteban"
+// Se usa Let
+var apellido="Piazza"
+// No se usa mas 
+
+const EDAD=29
+// Nunca voy a poder cambiar el valor que alla aca
+
+//camelCase-->VAMOS A ESCRIBIR TODO ACA
+
+//snake_case
+//kebab-case
+//l33t
+
+alert("algo inteligente")
+let edadUsuario=prompt("Ingrese su edad")
+//para pedir informacion al ingresar a la pagina
+
+//NaN hay uno de los valores q no es numerico 
+
+//int Numero=10
+//str nombre="esnteba"
+
+//true - false:
+
+//if()
+
+//console.log("ruso" &&)
+
+
+let edadMinima= 17
+
+console.log(++edadMinima)
+// me va tirar 18
+
+let edadARestar = prompt("ingres su edad")
+console.log(typeof(edadARestar))
+// Todos los imputs son strings
+
+alert("se edad el año" + (edadARestar+1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
