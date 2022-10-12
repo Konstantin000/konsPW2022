91968a27c72c6bfdd726618307e5d73c288f985b
Konstantin000 <kambrossenkov@itba.edu.ar>
2022-10-11 23:28:07 -0300

    ind

---
+++ Clase 11/script.js
@@ -0,0 +1,53 @@
+// objeto literal
+
+let profesor1={nombre:"ruso",apellido:"ambr" ,rol:"tata", edad . enActividad:true}
+
+//par clave:valor
+
+
+console.log(profesor1.nombre)
+//Este lo ves 
+
+
+console.table(profesor1)
+
+
+
+let usuario={
+    id:"",
+    Nombre:""
+
+}
+
+
+//Arrays/vector/lista , si lo anterior era una casa esto es un edificio
+
+const nombre=["ruso","juan","toro"]
+console.log(NOMBRES)
+console.log(NOMBRES[1])
+console.log(NOMBRE[0])
+
+
+console.log
+
+
+const CARRITO=[]
+console.log(CARRITO)
+CARRITO.push("hola")
+console.log(CARRITO)
+CARRITO.push("cahu")//agrega un elemento al final
+console.log(CARRITO)
+CARRITO.pop()//elimina el ultimo elemento
+console.log(CARRITO)
+CARRITO.unishift("unisheft")//lo agrega al principio
+console.log(CARRITO)
+CARRITO.shift()//elimina el primer elemento
+console.log(CARRITO.length)
+
+//psuh -pop
+//shift-unsift
+//Spice -slice
+//Join-toString
+//indexOf- lastIndex-includes
+
+
